d2bd6f54e2e059457dfcc9971918a3712f9af65d
kazuyakurashima <gs.kaz224@gmail.com>
2025-06-21 11:33:54 +0900

    完了ボタンで削除する実装。ただし、それが完了ボタンの方へ遷移がされていない。これは今日中にやる！

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -21,6 +21,18 @@ const onClickAdd = () => {
   setTodoText("");
 };
 
+const onClickDelete = (index) => {
+  const newTodos = [...incompleteTodos]
+  newTodos.splice(index, 1);
+  setIncompleteTodos(newTodos);
+};
+
+const onClickComplete = (index) => {
+  const newTodos = [...incompleteTodos]
+  newTodos.splice(index, 1);
+  setIncompleteTodos(newTodos);
+};
+
   return (
   <>
     <div className="input-area">
@@ -31,12 +43,12 @@ const onClickAdd = () => {
     <div className="incomplete-area">
       <p className="title">未完了のTODO</p>
       <ul>
-        {incompleteTodos.map((todo) =>  (
+        {incompleteTodos.map((todo, index) =>  (
           <li key={todo}>
             <div className="list-row">
               <p className="todo-item">{todo}</p>
-              <button>完了</button>
-              <button>削除</button>
+              <button onClick={() => onClickComplete(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
             </div>
           </li>
           )
